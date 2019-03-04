b7f756579097592bf6c495c292c7d6ac0b4a42f9
Guillermo Arria-Devoe <garri009@fiu.edu>
2019-03-04 13:11:48 -0500

    Updated action creators, previously wrong

--- src/actions/index.js
+++ src/actions/index.js
@@ -10,9 +10,9 @@ export const DECREMENT = 'DECREMENT';
 // is left to the reducer(s).
 
 export const increment = () => {
-  type: INCREMENT
+  return {type: INCREMENT}
 };
 
 export const decrement = () => {
-  type: DECREMENT
+  return {type: DECREMENT}
 };
